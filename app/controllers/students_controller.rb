92ef379ecd5664f57398656baeb086b93e9c0964
paolov1928 <ventura.paolo@gmail.com>
2018-11-26 09:15:20 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,31 @@
 class StudentsController < ApplicationController
-  
-  def index
-    @students = Student.all
-  end
 
-  def show
-    @student = Student.find(params[:id])
-  end
+  before_action :set_student, only: :show
+
+   def index
+     @students = Student.all
+   end
+
+   def show
+   end
+
+   def new
+   end
+
+   def create
+     session[:form_params] = params.inspect
+     redirect_to new_student_path
+   end
+
+   private
+
+     def set_student
+       @student = Student.find(params[:id])
+     end
+
+  # def create
+  #   @student = Student.create(first_name: params["student"]["first_name"], last_name: params["student"]["last_name"])
+  #   redirect_to student_path(@student)
+  # end
 
 end
